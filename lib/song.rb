231573f0b63ac6b67fe7bfd50c9fd0d6477da2bb
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 23:50:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,10 @@ class Song
     end
     
     def self.create_from_filename(name)
-      
+      song = self.new
+      song.name = (name.split(" - ")[1].chomp(".mp3"))
+      song.artist_name = (name.split(" - ")[0])
+      self.all << song
     end
     
 end
