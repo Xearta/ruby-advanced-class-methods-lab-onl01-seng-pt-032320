64abf184d437c09bd8657dfd9f4908b2ef046b2e
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 23:38:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,11 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    self.create_by_name(name) if not self.find_by_name(name)
+    if self.find_by_name(name)
+      self.name
+    else
+      self.create_by_name(name)
+    end
     
   end
     
